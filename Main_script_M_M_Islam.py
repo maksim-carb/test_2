4da7e200ee0fb7d55fc0ff5da9e0877b5e42e9c6
maksim-carb <mohammad.islam@arb.ca.gov>
2023-08-07 14:42:40 -0700

    after_I_ran_the_script

--- Main_script_M_M_Islam.py
+++ Main_script_M_M_Islam.py
@@ -1,11 +1,4 @@
 import pandas as pd
-from matplotlib import pyplot as plt
-import numpy as np
-# import statsmodels.api as sm
-from sklearn.linear_model import LinearRegression  
-
-from sklearn.ensemble import RandomForestRegressor
-import sklearn.metrics as metrics
 
 # from Supplementary_script_class_Linear_Reg_Diagnostic import Linear_Reg_Diagnostic
 
